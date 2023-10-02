fb8f2369d29761603cfab41e98e74c5da3793aea
Matteo Cardellini <matteo.cardellini@surgiq.com>
2023-10-02 12:18:15 +0200

    Finished with creation of latex pdf

--- benchmarks/experiments/delta/table-delta.py
+++ benchmarks/experiments/delta/table-delta.py
@@ -1,14 +1,15 @@
 import csv
 import os
-import re
 import shutil
-import time
+import statistics
 from typing import Dict, List
 
-from matplotlib import pyplot as plt
-from matplotlib.axes import Subplot
-
 from classes.Result import Result
+
+
+def rString(fValue, n):
+    return '{:.{n}f}'.format(fValue, n=n)
+
 
 DOMAINS = ["Baxter",
            "Descent",
@@ -31,11 +32,10 @@ HEURISTICS = [
     # "ENHSP-OPT-HMAX",
     "ENHSP-OPT-AIBR",
     # "ENHSP-OPT-HRADD",
-    "ENHSP-OPT-BLIND",
-    "PORTFOLIO"
+    "ENHSP-OPT-BLIND"
 ]
 
-LINES = {
+DELTAS = {
     ("NODELTA", "-de 0.1 -dp 0.1 -dh 0.1"): {
         "id": "1DELTA",
         "name": r"$1\delta$",
@@ -62,7 +62,7 @@ def main():
     file = f"benchmarks/results/{filename}"
 
     exp = filename.replace(".csv", "")
-    folder = f'benchmarks/figures/{exp}'
+    folder = f'benchmarks/latex/{exp}'
     if os.path.exists(folder):
         shutil.rmtree(folder)
     os.mkdir(folder)
@@ -77,63 +77,114 @@ def main():
     rDict: Dict[str, Dict[str, Dict[str, List[Result]]]] = dict()
     portfolio: Dict[str, Dict[str, Dict[str, List[Result]]]] = dict()
 
+    TIMEOUT = 0
     for r in results:
         domain = "-".join(r.domain.split("-")[:-1])
-        type = r.domain.split("-")[-1]
+        domainType = r.domain.split("-")[-1]
         solver = r.solver.split("[")[0]
         config = r.solver.split("[")[1][:-1]
-        line = LINES[(type, config)]
-        id = line["id"]
+        line = DELTAS[(domainType, config)]
+        lineId = line["id"]
         rDict[domain] = rDict.setdefault(domain, {})
         rDict[domain][solver] = rDict[domain].setdefault(solver, {})
-        rDict[domain][solver][id] = rDict[domain][solver].setdefault(id, [])
-        rDict[domain][solver][id].append(r)
+        rDict[domain][solver][lineId] = rDict[domain][solver].setdefault(lineId, [])
+        rDict[domain][solver][lineId].append(r)
 
-        portfolio[domain] = portfolio.setdefault(domain, {})
-        portfolio[domain][id] = portfolio[domain].setdefault(id, {})
-        portfolio[domain][id][r.problem] = portfolio[domain][id].setdefault(r.problem, [])
-        portfolio[domain][id][r.problem].append(r)
+        if r.time > TIMEOUT:
+            TIMEOUT = r.time
 
-        r.time = r.time if r.solved else 60000
-
+    table = dict()
     for domain in DOMAINS:
-        for line in LINES.values():
-            id = line["id"]
-            for rs in portfolio[domain][id].values():
-                r = Result.portfolio(rs, "PORTFOLIO")
-                rDict[domain]["PORTFOLIO"] = rDict[domain].setdefault("PORTFOLIO", {})
-                rDict[domain]["PORTFOLIO"][id] = rDict[domain]["PORTFOLIO"].setdefault(id, [])
-                rDict[domain]["PORTFOLIO"][id].append(r)
+        table[domain] = dict()
+        for heuristic in HEURISTICS:
+            table[domain][heuristic] = dict()
+            for delta in DELTAS.values():
+                deltaId = delta["id"]
+                table[domain][heuristic][deltaId] = dict()
+                row = table[domain][heuristic][deltaId]
+                elements = rDict[domain][heuristic][deltaId]
+                time = statistics.mean([r.time if r.solved else TIMEOUT for r in elements])
+                coverage = sum([1 if r.solved else 0 for r in elements]) / len(elements)
+                row["coverage"] = rString(coverage * 100, 2) if coverage > 0 else "-"
+                row["time"] = rString(time / 1000, 2) if coverage > 0 else "-"
 
-    plt.rcParams.update({
-        "text.usetex": True,
-        "figure.figsize": [7.50, 2.5 * len(HEURISTICS)],
-        "figure.autolayout": True
-    })
+    PROPERTIES = [("time", "RT (s)", -1), ("coverage", "Cov. (\%)", +1)]
+    best = dict()
+    for domain in DOMAINS:
+        best[domain] = dict()
+        for heuristic in HEURISTICS:
+            best[domain][heuristic] = dict()
+            for p in PROPERTIES:
+                bestValue = float("-inf") if p[2] > 0 else float("+inf")
+                bestItems = {}
+                for delta in DELTAS.values():
+                    deltaId = delta["id"]
+                    value = table[domain][heuristic][deltaId][p[0]]
+                    if value in {"-"}:
+                        continue
+                    if float(value) * p[2] > bestValue * p[2]:
+                        bestItems = {deltaId}
+                        bestValue = float(value)
+                    elif float(value) == bestValue:
+                        bestItems |= {deltaId}
 
-    for i, domain in enumerate(DOMAINS):
-        plt.figure(num=i, figsize=(20, 5))
-        plt.title(rf"${domain}$")
-        fig, axs = plt.subplots(len(HEURISTICS), 1)
-        for j, heuristic in enumerate(HEURISTICS):
-            ax: Subplot = axs[j]
-            ax.set_title(rf"{heuristic}")
-            ax.grid()
-            ax.set_xlabel("Scaling factor")
-            ax.set_ylabel("Pl. Time (s)")
-            # ax.set_xscale("log")
+                best[domain][heuristic][p[0]] = bestItems
 
-            for line in LINES.values():
-                points = dict({(i, r.time / 1000) for i, r in enumerate(rDict[domain][heuristic][line["id"]])})
-                x = list(points.keys())
-                x.sort()
-                y = [points[e] for e in x]
-                ax.plot(x, y, label=line["name"])
-                pass
+    latex = list()
+    latex.append(r"""
+        \documentclass[11pt,landscape]{article}
+        \usepackage{graphicx}
+        \usepackage{lscape}
+        \usepackage{multirow}
+        \usepackage[a4paper,margin=1in,landscape]{geometry}
+        
+        \begin{document}
+    
+        \begin{table*}[tb]
+        \centering
+        \resizebox{\textwidth}{!}{""")
 
-            ax.legend(loc="upper right")
-        plt.savefig(f'{folder}/{domain}-{exp}.pdf')
-        plt.show()
+    cArray = ["cc"] + ["|ccc" for d in DOMAINS]
+    latex.append(r"\begin{tabular}{" + ''.join(cArray) + "}")
+
+    dString = [r"\multicolumn{3}{|c}{" + d + "}" for d in DOMAINS]
+    latex.append(fr" & & " + "&".join(dString) + r"\\")
+    lString = "&".join([l["name"] for l in DELTAS.values()] * len(DOMAINS))
+    latex.append(fr"& & " + lString + r"\\")
+
+    for h in HEURISTICS:
+        latex.append(r"\hline")
+        for pi, p in enumerate(PROPERTIES):
+            hName = r"\multirow{2}{*}{" + h + "}" if pi == 0 else ""
+            row = [hName] + [p[1]]
+            for d in DOMAINS:
+                for delta in DELTAS.values():
+                    v = table[d][h][delta["id"]][p[0]]
+                    strV = r"\textbf{" + v + "}" if delta["id"] in best[d][h][p[0]] else v
+                    row.append(strV)
+            latex.append("&".join(row) + r"\\")
+
+    latex.append(r"""
+        \end{tabular}} \caption{Average runtime (RT, CPU-time seconds) and coverage (Cov.) achieved by 
+    informed and uninformed search approaches implemented in ENHSP (E) and UPMurphi (U) while relying on different 
+    discretisation approaches on well-known benchmark domains. Average runtime (RT) considers unsolved instances as 
+    cut-off time (300 seconds).} 
+        \label{tab:single-static} 
+        \end{table*}  
+        \end{document}""")
+    latexStr = "\n".join(latex)
+
+    exp = filename.replace(".csv", "")
+    folder = f'benchmarks/latex/{exp}'
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+    os.mkdir(folder)
+    with open(f"{folder}/{exp}.tex", "w") as f:
+        f.write(latexStr)
+    os.system(f"pdflatex -interaction=nonstopmode --output-directory='{folder}' {folder}/{exp}.tex ")
+
+    os.remove(f"{folder}/{exp}.aux")
+    os.remove(f"{folder}/{exp}.log")
 
 
 if __name__ == '__main__':
