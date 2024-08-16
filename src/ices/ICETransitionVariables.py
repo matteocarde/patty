00a3bba7f4e13b9908fd0b2e2a0f24b8a5a4be38
Matteo Cardellini <matteo.cardellini@surgiq.com>
2024-08-16 10:46:54 +0200

    Starting with ICE Encoding

---
+++ src/ices/ICETransitionVariables.py
@@ -0,0 +1,8 @@
+from src.ices.ICEPattern import ICEPattern
+from src.ices.ICETask import ICETask
+
+
+class ICETransitionVariables:
+
+    def __init__(self, task: ICETask, pattern: ICEPattern):
+        pass
